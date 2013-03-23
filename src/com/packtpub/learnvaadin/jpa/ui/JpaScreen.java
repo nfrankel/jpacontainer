09a6dd68a4e21a8e420ebdf70dc55a1198d2b47e
Nicolas Frankel <nicolas@frankel.ch>
2013-03-23 15:21:12 +0100

    Replaced column decorator with nested property

--- src/com/packtpub/learnvaadin/jpa/ui/JpaScreen.java
+++ src/com/packtpub/learnvaadin/jpa/ui/JpaScreen.java
@@ -2,13 +2,10 @@ package com.packtpub.learnvaadin.jpa.ui;
 
 import java.util.Locale;
 
-import com.packtpub.learnvaadin.jpa.model.Job;
 import com.packtpub.learnvaadin.jpa.model.Person;
-import com.vaadin.addon.jpacontainer.EntityItem;
 import com.vaadin.addon.jpacontainer.JPAContainer;
 import com.vaadin.addon.jpacontainer.JPAContainerFactory;
 import com.vaadin.ui.Table;
-import com.vaadin.ui.Table.ColumnGenerator;
 import com.vaadin.ui.VerticalLayout;
 
 @SuppressWarnings("serial")
@@ -20,26 +17,14 @@ public class JpaScreen extends VerticalLayout {
 
 		JPAContainer<Person> container = JPAContainerFactory.make(Person.class, "vaadindemo");
 
+		container.addNestedContainerProperty("job.label");
+
 		Table table = new Table("", container);
 
 		table.setLocale(Locale.US);
 		table.setPageLength(4);
 
-		table.setVisibleColumns(new Object[] { "lastName", "firstName", "birthdate", "job" });
-
-		table.addGeneratedColumn("job", new ColumnGenerator() {
-
-			@Override
-			public Object generateCell(Table source, Object itemId, Object columnId) {
-
-				@SuppressWarnings({ "unchecked", "rawtypes" })
-				EntityItem<Person> item = (EntityItem) source.getItem(itemId);
-
-				Job job = item.getEntity().getJob();
-
-				return job == null ? null : job.getLabel();
-			}
-		});
+		table.setVisibleColumns(new Object[] { "lastName", "firstName", "birthdate", "job.label" });
 
 		addComponent(table);
 	}
